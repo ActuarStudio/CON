723d1e92366474e6944de97a7c21a08d5f19b223
Volodymyr <vl.mirsk@gmail.com>
2021-03-06 23:36:29 +0200

    123

--- sl.py
+++ sl.py
@@ -1,13 +1,67 @@
 import streamlit as st
 from st_aggrid import AgGrid
 import pandas as pd
+import util
 
-w = st.file_uploader("Upload a excel file", type="xlsx")
+st.set_page_config(
+    page_title="ActuarOnline",
+    page_icon="🧊",                       # EP: how did they find a symbol?
+    layout='centered',                    #"wide",
+    initial_sidebar_state='collapsed'     #"expanded",
+)
+
+#################################
+
+#Убрать меню и фуктер
+#hide_streamlit_style = """
+#<style>
+#    #MainMenu {visibility: hidden;}
+#    footer {visibility: hidden;}
+#</style>
+#"""
+hide_streamlit_style = """
+<style>
+    footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+#util.localCSS("style2.css")
+util.localCSS("style.css")
+
+util.genHeroSection(
+    title1="Actuar",
+    title2="Studio",
+    subtitle="Актуарний Online сервіс від Actuar.studio",
+    logo= util.load_image("LOGO.png"),
+    header=True
+)
+
+st.write(f"""<div>
+        <div class="base-wrapper flex flex-column" style="background-color:#0090A7">
+            <div class="white-span header p1" style="font-size:30px;">Перевір корректність НБУ XML</div>
+            <div class="white-span header p1" style="font-size:30px;">Ознайомся з вмістом</div>
+            <div class="white-span header p1" style="font-size:30px;">Конвертуй у XLSX формат</div>
+    </div>""",
+         unsafe_allow_html=True,
+         )
+
+st.write(
+    """
+<div class="base-wrapper primary-span">
+    <span class="section-header">Конвертор НБУ XML файлів</span>
+</div>""",
+    unsafe_allow_html=True,
+)
+
+#################################
+
+w = st.file_uploader("Оберіть XML файл звітності НБУ", type="xml")
 if w:
     data = pd.read_excel(w)
     st.write(data)
 
-st.title("IBNR")
+
 
 d = pd.to_datetime("now")
 d = pd.Period(d, freq='Q') - 1
@@ -17,41 +71,23 @@ st.sidebar.file_uploader('Введіть Журнал виплат',['xls','xlsx
 st.sidebar.file_uploader('Введіть Журнал зароблених премій',['xls','xlsx'])
 st.sidebar.file_uploader('Введіть Журнал збитковості',['xls','xlsx'])
 
-ddd = st.text('Loading data...')
-ddd.text('Loading data...2')
+
+##############################
+
 data_load_state = st.text('Loading data....3')
 
-f=st.file_uploader('File uploader')
-
+f ='https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv'
 href = f'<a href="data:file/csv;base64,{f}">Download csv file</a>'
 st.markdown(href, unsafe_allow_html=True)
 data_load_state.text('Loading data...done!')
 
-
-st.title("Lists!")
-
-lists = [
-    [],
-    [10, 20, 30],
-    [[10, 20, 30], [1, 2, 3]],
-    [[10, 20, 30], [1]],
-    [[10, "hi", 30], [1]],
-    [[{"foo": "bar"}, "hi", 30], [1]],
-    [[{"foo": "bar"}, "hi", 30], [1, [100, 200, 300, 400]]],
-]
-
-
-for i, l in enumerate(lists):
-    st.header("List %d" % i)
-
-    st.write("With st.write")
-    st.write(l)
-
-    st.write("With st.json")
-    st.json(l)
-
-    st.write("With st.dataframe")
-    st.dataframe(l)
-
-df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
+df = pd.read_csv(f)
 AgGrid(df)
+
+#################################
+
+st.write(f"""
+        <div class="base-wrapper flex flex-column" style="background-color:#0090A7">
+            <div class="white-span header p1" style="font-size:30px;">Actuar.Studio 2021</div>
+        </div>""",
+         unsafe_allow_html=True, )
